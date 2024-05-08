73a32b25c5cac4e2ad353bb4d05358310e471d21
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2024-05-09 08:44:47 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,25 +1,14 @@
 class Solution {
     public int solution(int[] num_list) {
         
-        int multiplication = getMultiplication(num_list);
-        int squaredSum = getSquaredSum(num_list);
-    
-        return multiplication < squaredSum ? 1 : 0;
-    }
-    
-    private int getMultiplication(int[] num_list){
         int multiplication = 1;
+        int squaredSum = 0;
+        
         for(int num : num_list){
             multiplication*=num;
+            squaredSum+=num;
         }
-        return multiplication;
-    }
     
-    private int getSquaredSum(int[] num_list){
-        int sum = 0;
-        for(int num : num_list){
-            sum+=num;
-        }
-        return (int)Math.pow(sum,2);
+        return multiplication < (int) Math.pow(squaredSum,2) ? 1 : 0;
     }
 }
