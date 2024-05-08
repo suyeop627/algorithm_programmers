8388697d8d50b54a8e9fa36a8fbc1666614cfea2
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2024-05-08 21:55:29 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 6.60 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,5 +1,5 @@
 class Solution {
     public int solution(String myString, String pat) {
-        return myString.toLowerCase().indexOf(pat.toLowerCase())<0 ? 0 : 1;
+        return myString.toLowerCase().contains(pat.toLowerCase()) ? 1 : 0;
     }
 }
