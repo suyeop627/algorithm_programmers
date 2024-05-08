f0120a0fe0abb56f6de2d75c745ecad5102d03d5
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2024-05-08 22:17:58 +0900

    [level 0] Title: 할 일 목록, Time: 0.63 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -2,7 +2,6 @@ import java.util.List;
 import java.util.ArrayList;
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        String[] answer = {};
         List<String> notFinishedList = new ArrayList<>();
         for(int i = 0; i<finished.length; i++){
             if(!finished[i]){
