3ba2bfd9b0e811eb848e15cde875c954af512b9e
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2024-05-09 09:48:49 +0900

    Delete 프로그래머스/1/12922. 수박수박수박수박수박수？ directory

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public String solution(int n) {
-        StringBuffer sb = new StringBuffer();
-        for(int i = 1; i<=n; i++){
-            sb.append(i%2==1 ? "수" : "박");
-        }
-        return sb.toString();
-    }
-}
