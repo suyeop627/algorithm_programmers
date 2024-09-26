0226cc4c62866f011e348b0a31c3d55e35df8ba5
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2024-09-26 11:00:50 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for(int i = 2; i<=n; i+=2){
-            answer+=i;
-        }
-        return answer;
+        int halfOfN = n/2;
+        return halfOfN * (halfOfN +1);
     }
 }
