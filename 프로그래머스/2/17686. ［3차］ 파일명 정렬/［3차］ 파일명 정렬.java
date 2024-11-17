c89a7290ecda8bf502a1820e57ea9618c5624e9f
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2024-11-17 16:37:46 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 21.14 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] files) {
+        return Arrays.stream(files)
+            .sorted((file1, file2) -> {
+                FileName part1 = splitFileName(file1);
+                FileName part2 = splitFileName(file2);
+
+                //head 비교
+                int headCompare = part1.head.compareToIgnoreCase(part2.head);
+                if (headCompare != 0) {
+                    return headCompare;
+                }
+                //number 비교(같은 경우, part1이 더 앞으로 오도록 등호조건 설정)
+                //return part1.number < part2.number ? -1 : 1;
+                 return Integer.compare(part1.number, part2.number);
+            })
+            .toArray(String[]::new);
+    }
+
+    // 파일명에서 HEAD, NUMBER, TAIL을 분리하는 메소드
+    private FileName splitFileName(String file) {
+        int headLength = 0;
+        //숫자 나오기 전까지
+        while (headLength < file.length() && !Character.isDigit(file.charAt(headLength))) {
+            headLength++;
+        }
+        String head = file.substring(0, headLength);
+
+        //headLength ~ 마지막 숫자까지
+        int numberLength = headLength;
+        while (numberLength < file.length() && Character.isDigit(file.charAt(numberLength))) {
+            numberLength++;
+        }
+        int number = Integer.parseInt(file.substring(headLength, numberLength));
+
+        return new FileName(head, number);
+    }
+
+    class FileName {
+        String head;
+        int number;
+
+        FileName(String head, int number) {
+            this.head = head;
+            this.number = number;           
+        }
+    }
+}
