91f5b068831723ce346f61b6acd867a62fb6e437
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2025-05-08 22:19:39 +0900

    [level 2] Title: 완전범죄, Time: 123.41 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -3,46 +3,47 @@ import java.util.Set;
 import java.util.HashSet;
 
 class Solution {
-    private int minATrace = 120;
-    
+    private int minTraceA = 120;
+
     private Set<String> stealState = new HashSet<>();
-    
+
     public int solution(int[][] info, int n, int m) {
         //큰거부터 훔치도록(시간초과 개선)
         Arrays.sort(info, (a, b) -> b[0] - a[0]);
-        
+
         steal(info, n, m, 0, 0, 0);
-        
-        return minATrace == 120 ? -1 : minATrace;
+
+        return minTraceA == 120 ? -1 : minTraceA;
     }
-    
-    private void steal(int[][] info, int n, int m, int sumOfATrace, int sumOfBTrace , int index){
+
+    private void steal(int[][] traceInfo, int traceLimitA, int traceLimitB, 
+                       int sumOfTraceA, int sumOfTraceB , int itemIndex){
         //둘중 하나 이상 걸림
-        if(sumOfATrace >= n || sumOfBTrace >= m){
+        if(sumOfTraceA >= traceLimitA || sumOfTraceB >= traceLimitB){
             return;
         }
         //이미 최소 흔적을 넘긴 경우 종료(시간초과 개선)
-        if(sumOfATrace >= minATrace){
+        if(sumOfTraceA >= minTraceA){
             return;
         }
         //index-A흔적합-B흔적합 중간 결과값 저장(시간초과 개선)
-        String currentState = String.format("%d|%d|%d", index,sumOfATrace, sumOfBTrace);
+        String currentState = String.format("%d|%d|%d", itemIndex,sumOfTraceA, sumOfTraceB);
         if(stealState.contains(currentState)){
             return;
         }
         stealState.add(currentState);
-        
+
         //전부 훔치면 종료
-        if(index == info.length){
-            minATrace = Math.min(sumOfATrace, minATrace);
+        if(itemIndex == traceInfo.length){
+            minTraceA = sumOfTraceA;
             return;
         }
-        
-        int nextIndex = index+1;
+
+        int nextItemIndex = itemIndex+1;
         //A 훔침
-        steal(info, n, m, sumOfATrace+info[index][0], sumOfBTrace, nextIndex);
-        
+        steal(traceInfo, traceLimitA, traceLimitB, sumOfTraceA+traceInfo[itemIndex][0], sumOfTraceB, nextItemIndex);
+
         //B 훔침
-        steal(info, n, m, sumOfATrace, sumOfBTrace+info[index][1], nextIndex);
+        steal(traceInfo, traceLimitA, traceLimitB, sumOfTraceA, sumOfTraceB+traceInfo[itemIndex][1], nextItemIndex);
     }
 }
