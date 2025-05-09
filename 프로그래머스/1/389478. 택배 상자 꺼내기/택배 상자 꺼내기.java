a53b6b7b687fe9084d91969259b81423e9a03caa
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2025-05-09 22:36:29 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 1.31 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution {
+   public int solution(int n, int w, int num) {
+        int answer = 1;
+        List<Stack<Integer>> warehouse = fillWarehouse(n, w);
+        Stack<Integer> targetStack = warehouse.stream().filter(q -> q.contains(num)).findAny().get();
+
+        while(!targetStack.pop().equals(num)){
+            answer++;
+        }
+        return answer;
+    }
+
+    private List<Stack<Integer>> fillWarehouse(int n, int w) {
+        List<Stack<Integer>> warehouse = new ArrayList<>();
+
+        for(int i = 0; i < w; i++){
+            warehouse.add(new Stack<>());
+        }
+
+        int tmp = n;
+        int order = 0;
+        outer:
+        for (int i = 0; i < n / w + 1; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < w; j++) {
+                    warehouse.get(j).push(++order);
+                    if (--tmp == 0) break outer;
+                }
+            } else {
+                for (int j = w - 1; j >= 0; j--) {
+                    warehouse.get(j).push(++order);
+                    if (--tmp == 0) break outer;
+                }
+            }
+        }
+        return warehouse;
+    }
+}
