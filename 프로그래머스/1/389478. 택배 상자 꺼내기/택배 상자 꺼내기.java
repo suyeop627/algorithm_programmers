f3bd49ffecfb8eedbb8ef8e1668c60b38b11f76b
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2025-05-12 21:04:54 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 2.50 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -3,11 +3,16 @@ class Solution {
    public int solution(int n, int w, int num) {
         int answer = 1;
         List<Stack<Integer>> warehouse = fillWarehouse(n, w);
-        Stack<Integer> targetStack = warehouse.stream().filter(q -> q.contains(num)).findAny().get();
+        
+        Stack<Integer> targetStack = warehouse.stream()
+                                              .filter(s -> s.contains(num))
+                                              .findAny()
+                                              .get();//무조건 존재할테니 그냥 get
 
         while(!targetStack.pop().equals(num)){
             answer++;
         }
+       
         return answer;
     }
 
