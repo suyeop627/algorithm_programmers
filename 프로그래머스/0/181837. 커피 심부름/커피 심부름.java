e386fbaaf84d5ea02f4af8c7ae2b8aaed4132126
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2025-07-05 02:41:09 +0900

    [level 0] Title: 커피 심부름, Time: 0.78 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for(String o : order){
+            if(o.contains("latte")) answer+=5000;    
+            else answer+=4500;
+        }
+        return answer;
+    }
+}
