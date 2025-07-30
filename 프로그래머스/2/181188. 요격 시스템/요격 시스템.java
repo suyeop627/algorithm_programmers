586553281b34f8e753e352bd83fc14335489e909
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2025-07-30 22:53:20 +0900

    [level 2] Title: 요격 시스템, Time: 386.24 ms, Memory: 201 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,19 +1,42 @@
 import java.util.Arrays;
 
 class Solution {
+
     public int solution(int[][] targets) {
-            int answer = 0;
+        int answer = 0;
 
-            Arrays.sort(targets, (a, b) -> a[1] - b[1]);
+        Arrays.sort(targets, (a, b) -> a[1] - b[1]);
 
-            int firePoint = -1;
+        //print(targets);
 
-            for (int[] target : targets) {
-                if (firePoint <= target[0]) {
-                    answer++;
-                    firePoint = target[1];
+        int firePoint = -1;
+
+        for (int[] target : targets) {
+            if (firePoint <= target[0]) {
+                answer++;
+                firePoint = target[1];
+            }
+        }
+        return answer;
+    }
+
+    private static void print(int[][] targets) {
+        for (int[] target : targets) {
+            int start = target[0] * 10;
+            int end = target[1] * 10;
+
+            for (int j = 0; j <= targets[targets.length - 1][1] * 10; j++) {
+                if (start == j) {
+                    System.out.print("o");
+                } else if (start < j && end > j) {
+                    System.out.print("-");
+                } else if (end == j) {
+                    System.out.print("o");
+                } else {
+                    System.out.print(" ");
                 }
             }
-            return answer;
+            System.out.println();
         }
+    }
 }
