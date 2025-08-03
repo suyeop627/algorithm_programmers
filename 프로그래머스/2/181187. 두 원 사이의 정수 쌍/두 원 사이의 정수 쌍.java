55ac83b9a51c678dffaab62d2901c7746ccd6e08
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2025-08-03 16:00:56 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 24.31 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -3,7 +3,9 @@ class Solution {
         long answer = 0;
 
         for (int x = 1; x <= r2; x++) {
-            int maxY = (int) Math.floor(Math.sqrt((long) r2 * r2 - (long) x * x));
+            //int로 downcasting 시 소수점 버림(floor)
+            int maxY = (int) Math.sqrt((long) r2 * r2 - (long) x * x);
+            //minY보다는 커야되므로 올림 처리
             int minY = (int) Math.ceil(Math.sqrt((long) r1 * r1 - (long) x * x));
             answer += (maxY - minY + 1);
         }
