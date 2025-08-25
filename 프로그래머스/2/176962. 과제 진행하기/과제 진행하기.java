98eb27c2705c0a17161bdcafc1a134360f320ab5
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2025-08-25 11:57:37 +0900

    [level 2] Title: 과제 진행하기, Time: 24.42 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -15,39 +15,42 @@ class Solution {
                 .collect(Collectors.toList());
 
         ArrayDeque<Task> stoppedTasks = new ArrayDeque<>();
-
+        //다음 과제가 존재할 경우에만 시간 비교를 하기 위해서 size -1
         for (int i = 0; i < tasks.size() - 1; i++) {
 
             Task nowTask = tasks.get(i);
             LocalTime nextStart = tasks.get(i + 1).start;
-
+            //현재 과제 시작시간부터 다음과제 시작시간까지의 여유시간
             int nowAvailableTime = (int) Duration.between(nowTask.start, nextStart).toMinutes();
 
+            //다음과제 시작 전까지 현재 과제를 완료할 수 있는 경우
             if (nowTask.remainingMinute <= nowAvailableTime) {
 
                 result.add(nowTask.name);
                 int freeTime = nowAvailableTime - nowTask.remainingMinute;
-
+                //다음과제 시작시간 전까지 미뤄둔 과제 있으면 처리 
                 while (!stoppedTasks.isEmpty() && freeTime > 0) {
                     Task stoppedTask = stoppedTasks.pop();
 
-                    if (stoppedTask.remainingMinute <= freeTime) {
+                    if (stoppedTask.remainingMinute <= freeTime) { //잔여시간동안 과제를 완료할 수 있는 경우
                         result.add(stoppedTask.name);
                         freeTime -= stoppedTask.remainingMinute;
-                    } else {
+                    } else {//잔여시간 동안 미뤄둔 과제를 완료하지 못할경우 과제의 잔여시간만 차감
                         stoppedTask.doTaskDuring(freeTime);
                         stoppedTasks.push(stoppedTask);
                         freeTime = 0;
                     }
                 }
             } else {
+                //할수있는 만큼 과제 진행 후 미뤄두기
                 nowTask.doTaskDuring(nowAvailableTime);
                 stoppedTasks.push(nowTask);
             }
 
         }
+        //마지막과제는 완료
         result.add(tasks.get(tasks.size()-1).name);
-
+        //미뤄둔 과제 하나씩 처리
         while (!stoppedTasks.isEmpty()) {
             result.add(stoppedTasks.pop().name);
         }
