9174e555afeaacd36163305d0d79961a3636f3e4
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2025-08-27 22:35:39 +0900

    [level 2] Title: 리코쳇 로봇, Time: 15.85 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,70 +1,70 @@
 import java.util.*;
+
 class Solution {
-    private final int[][] direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     private final int[] robot = new int[2];
-    
+
     public int solution(String[] board) {
 
-        String[][] map = initGame(board);
+        char[][] map = initGame(board);
         Deque<int[]> queue = new ArrayDeque<>();
         Set<String> visited = new HashSet<>();
-        
+
         queue.offer(new int[]{robot[0], robot[1], 0});
         return bfs(map, queue, visited);
     }
-    
-    private int bfs(String[][] map, Deque<int[]> queue, Set<String> visited){
-        while(!queue.isEmpty()){
-                
+
+    private int bfs(char[][] map, Deque<int[]> queue, Set<String> visited) {
+        while (!queue.isEmpty()) {
+
             int[] now = queue.poll();
             int y = now[0];
             int x = now[1];
-            int loopCount = now[2];
-            
-            if (map[y][x].equals("G")) {
-                return loopCount;
+            int moveCount = now[2];
+
+            if (map[y][x]=='G') {
+                return moveCount;
             }
-            
-            for(int[] d : direction){
-                int ny = y;
-                int nx = x;
-                while(true){
-                    //ny, nx 그대로 쓰면 while 탈출할때 처리 애매해짐 -> 임시값 할당해서 처리
-                    int ty = ny+d[0];
-                    int tx = nx+d[1];
-                    
-                    if (ty < 0 || ty >= map.length || tx < 0 || tx >= map[0].length 
-                        || map[ty][tx].equals("D")) {
-                        break;
-                    }
-                    ny=ty;
-                    nx=tx;
-                }   
-                // System.out.println("ny = "+ny + " , nx = "+nx);
-                String key = ny + "|" + nx;
+
+            for (int[] direction : DIRECTIONS) {
+                int[] nextPosition = slideRobot(y, x, direction, map);
+                String key = nextPosition[0] + "|" + nextPosition[1];
                 if (!visited.contains(key)) {
                     visited.add(key);
-                    queue.offer(new int[]{ny, nx, loopCount + 1});
+                    queue.offer(new int[]{nextPosition[0], nextPosition[1], moveCount + 1});
                 }
             }
-            
         }
-        
-        
+
         return -1;
     }
-    
-    private String[][] initGame(String[] board){
-        String[][] map = new String[board.length][];
-        for(int i = 0; i < board.length; i++){
-            if(board[i].contains("R")){
+
+    private int[] slideRobot(int y, int x, int[] direction, char[][] map) {
+        int[] nextPosition = new int[]{y, x};
+        while (true) {
+            int ty = nextPosition[0] + direction[0];
+            int tx = nextPosition[1] + direction[1];
+
+            if (ty < 0 || ty >= map.length || tx < 0 || tx >= map[0].length || map[ty][tx]==('D')) {
+                break;
+            }
+            nextPosition[0] = ty;
+            nextPosition[1] = tx;
+        }
+        return nextPosition;
+    }
+
+    private char[][] initGame(String[] board) {
+        char[][] map = new char[board.length][];
+        for (int i = 0; i < board.length; i++) {
+            if (board[i].contains("R")) {
                 robot[0] = i;
                 robot[1] = board[i].indexOf("R");
             }
-                
-            map[i] = board[i].split("");
+
+            map[i] = board[i].toCharArray();
         }
         return map;
-            
+
     }
 }
