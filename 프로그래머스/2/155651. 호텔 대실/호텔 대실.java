a883f9968be2af3766468e9120bbec9d424ce0b3
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2025-10-01 11:03:42 +0900

    [level 2] Title: 호텔 대실, Time: 23.00 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,7 +1,6 @@
 import java.time.LocalTime;
 import java.util.*;
 
-
 class Solution {
     public int solution(String[][] book_time) {
         List<Booking> bookings = new ArrayList<>();
@@ -12,17 +11,17 @@ class Solution {
 
         bookings.sort(Comparator.comparing(b -> b.startTime));
 
-        //우선순위가 높은 요소가 먼저 나가는 큐
-        // - 우선순위 : compareTo 혹은 Comparator로 정해지는 정렬 순서에서 더 앞서는 객체
-        //PriorityQueue<LocalTime> roomCleaningEndTimes = new PriorityQueue<>(LocalTime::compareTo); - 불필요함
+    //우선순위가 높은 요소가 먼저 나가는 큐
+    // - 우선순위 : compareTo 혹은 Comparator로 정해지는 정렬 순서에서 더 앞서는 객체
+    //PriorityQueue<LocalTime> roomCleaningEndTimes = new PriorityQueue<>(LocalTime::compareTo);
         PriorityQueue<LocalTime> roomCleaningEndTimes = new PriorityQueue<>();
 
         int maxRooms = 0;
-        
+
         for (Booking booking : bookings) {
             removeRoomsReady(booking, roomCleaningEndTimes);
 
-            LocalTime roomCleaningEndtime = booking.endTime.plusMinutes(10);
+            LocalTime roomCleaningEndtime = getRoomCleaningEndtime(booking);
             roomCleaningEndTimes.add(roomCleaningEndtime);
 
             maxRooms = Math.max(maxRooms, roomCleaningEndTimes.size());
@@ -31,15 +30,27 @@ class Solution {
         return maxRooms;
     }
 
-    private void removeRoomsReady(Booking booking, PriorityQueue<LocalTime> roomCleaningEndTimes) {
+    private void removeRoomsReady(Booking booking, 
+                                  PriorityQueue<LocalTime> roomCleaningEndTimes) {
+        
         //10분 퇴실 + 청소 10분 | 20분 입실 - 가능해야함
-        while (!roomCleaningEndTimes.isEmpty() && !roomCleaningEndTimes.peek().isAfter(booking.startTime)) {
+        while (!roomCleaningEndTimes.isEmpty() 
+               && !roomCleaningEndTimes.peek().isAfter(booking.startTime)) {
             roomCleaningEndTimes.poll();
         }
     }
 
+    private LocalTime getRoomCleaningEndtime(Booking booking) {
+        LocalTime bookingEndTime = booking.endTime;
+        //예약 종료시간 + 청소시간이 자정 넘어가서 실패함 -> 자정 직전으로 지정
+        if (!bookingEndTime.isBefore(LocalTime.of(23, 50))) {
+            return LocalTime.of(23, 59);
+        }
+
+        return bookingEndTime.plusMinutes(10);
+    }
+
     static class Booking {
-        
         LocalTime startTime;
         LocalTime endTime;
 
@@ -50,7 +61,8 @@ class Solution {
 
         private static LocalTime convertStringToLocalTime(String stringTime) {
             String[] hourAndMinute = stringTime.split(":");
-            return LocalTime.of(Integer.parseInt(hourAndMinute[0]), Integer.parseInt(hourAndMinute[1]));
+            return LocalTime.of(Integer.parseInt(hourAndMinute[0]), 
+                                Integer.parseInt(hourAndMinute[1]));
         }
     }
 }
