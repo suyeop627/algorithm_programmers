0b804be10e482635911c4b64f6ea38e9819526a3
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2025-12-31 11:12:12 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.93 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -4,20 +4,15 @@ import java.util.Comparator;
 
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         
         for(int i = 1; i < food.length; i++){
             int q = food[i] / 2;
             for(int j = 0; j < q; j++){
-                answer += i;
+                sb.append(i);
             }
         }
         
-        // System.out.println(answer);
-        
-        StringBuilder sb = new StringBuilder(answer);
-        
-        
-        return answer + "0" + sb.reverse().toString();
+        return sb.toString() + "0" + sb.reverse().toString();
     }
 }
