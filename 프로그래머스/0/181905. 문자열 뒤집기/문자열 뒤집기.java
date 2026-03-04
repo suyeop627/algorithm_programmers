1364c09f4452a52e18e6427b2cbfd6c693410076
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2026-03-04 21:35:45 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.09 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,14 +1,16 @@
 import java.lang.StringBuilder;
 class Solution {
     public String solution(String my_string, int s, int e) {
-        String front = my_string.substring(0, s);
-        String middle = my_string.substring(s, e+1);
-        String back = my_string.substring(e+1);
+        char[] arr = my_string.toCharArray();
         
+        while (s < e) {
+            char temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+            s++;
+            e--;
+        }
         
-        StringBuilder sb = new StringBuilder(middle);
-        middle = sb.reverse().toString();
-        
-        return front+middle+back;
+        return String.valueOf(arr);
     }
 }
