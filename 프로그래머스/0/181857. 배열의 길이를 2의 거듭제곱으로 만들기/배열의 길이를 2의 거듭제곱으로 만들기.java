a64e8cde22a6d7523fe5a35217ae3951fde2fa05
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2026-03-04 21:57:41 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.05 ms, Memory: 91.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        
+        int targetLength = 1;
+        while(targetLength < arr.length){
+           targetLength *=2; 
+        }
+        int[] answer = new int[targetLength];
+        for(int i = 0; i < arr.length; i++){
+            answer[i] = arr[i];
+        }
+        
+        
+        return answer;
+    }
+}
