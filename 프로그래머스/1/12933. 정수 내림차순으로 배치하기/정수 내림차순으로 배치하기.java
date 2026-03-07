3ce46bdef5f5dd81ceb26a8103e5ce65cace1d17
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2026-03-07 19:41:17 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        int[] arr = new int[10];
+        while(n >0){
+            arr[(int)(n%10)]++;
+            n/=10;
+        }
+
+        for(int i = 9; i >= 0; i--){
+            for(int j = 0; j < arr[i]; j++){
+                answer = answer*10 + i;
+            }
+        }
+        
+        return answer;
+    }
+}
