cc7202cd6cbd61c9d1855c706db50c07431622be
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2026-03-08 22:26:07 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[][] score) {
+        int[] answer = new int[score.length];
+        double[] avg = new double[score.length];
+        for(int i = 0; i < score.length; i++){
+            avg[i] = (score[i][0] + score[i][1])/2.0;
+        }
+        for(int i = 0; i < avg.length; i++){
+            int rank = 1;
+            for(int j = 0; j < avg.length; j++){
+                if(avg[j] > avg[i]){
+                    rank++;
+                }
+            }
+            answer[i] = rank;
+        }
+        return answer;
+    }
+}
